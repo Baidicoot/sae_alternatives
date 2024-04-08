f6df13465fddcae9436d5260fde7d0c34afad8c5
Aidan Ewart <aidanprattewart@gmail.com>
2024-04-08 15:11:38 +0000

    hhh

--- modelling_sae.py
+++ modelling_sae.py
@@ -4,6 +4,12 @@ def normalize_dict(m):
     norms = torch.norm(m, p=2, dim=-1).unsqueeze(-1)
     return m / norms
 
+def huber_loss(x, x_hat, delta):
+    raw_loss = (x - x_hat)
+    loss = raw_loss.pow(2) * 0.5
+    loss[loss > delta] = delta * (raw_loss.abs() - 0.5 * delta)
+    return loss.mean()
+
 class BasicSAE(torch.nn.Module):
     def __init__(self, d_in, d_hidden, alpha):
         super().__init__()
@@ -22,9 +28,9 @@ class BasicSAE(torch.nn.Module):
         x_hat = torch.einsum('ij,bi->bj', normalize_dict(self.unembed), h) + self.unembed_bias.unsqueeze(0)
 
         mse = (x - x_hat).pow(2).mean()
-        sparsity = torch.norm(h, 1, dim=-1).mean() * self.alpha
+        sparsity = h.abs().mean()
 
-        return mse + sparsity, mse, h, x_hat
+        return mse + sparsity * self.alpha, mse, sparsity, h, x_hat
 
     @property
     def dictionary(self):
@@ -48,9 +54,9 @@ class SoftplusSAE(torch.nn.Module):
         x_hat = torch.einsum('ij,bi->bj', normalize_dict(self.unembed), h) + self.unembed_bias.unsqueeze(0)
 
         mse = (x - x_hat).pow(2).mean()
-        sparsity = torch.norm(h, 1, dim=-1).mean() * self.alpha
+        sparsity = h.abs().mean()
 
-        return mse + sparsity, mse, h, x_hat
+        return mse + sparsity * self.alpha, mse, sparsity, h, x_hat
 
     @property
     def dictionary(self):
@@ -67,13 +73,87 @@ class RICA(torch.nn.Module):
         self.alpha = alpha
     
     def forward(self, x):
-        h = torch.einsum('ij,bj->bi', self.embed, x + self.embed_bias.unsqueeze(0))
-        x_hat = torch.einsum('ij,bi->bj', self.embed, h) - self.embed_bias.unsqueeze(0)
+        h = torch.einsum('ij,bj->bi', self.embed, x - self.embed_bias.unsqueeze(0))
+        x_hat = torch.einsum('ij,bi->bj', self.embed, h) + self.embed_bias.unsqueeze(0)
 
         mse = (x - x_hat).pow(2).mean()
-        sparsity = torch.norm(h, 1, dim=-1).mean() * self.alpha
+        sparsity = h.abs().mean()
 
-        return mse + sparsity, mse, h, x_hat
+        return mse + sparsity * self.alpha, mse, sparsity, h, x_hat
+
+    @property
+    def dictionary(self):
+        return normalize_dict(self.embed).detach()
+
+class HuberRICA(torch.nn.Module):
+    def __init__(self, d_in, d_hidden, alpha):
+        super().__init__()
+        initial_matrix = torch.randn(d_hidden, d_in)
+        initial_matrix = normalize_dict(initial_matrix)
+        self.embed = torch.nn.Parameter(initial_matrix.clone())
+        self.embed_bias = torch.nn.Parameter(torch.zeros(d_in))
+
+        self.alpha = alpha
+    
+    def forward(self, x):
+        h = torch.einsum('ij,bj->bi', self.embed, x - self.embed_bias.unsqueeze(0))
+        x_hat = torch.einsum('ij,bi->bj', self.embed, h) + self.embed_bias.unsqueeze(0)
+
+        huber = huber_loss(x, x_hat, 1)
+        mse = (x - x_hat).pow(2).mean()
+        sparsity = h.abs().mean()
+
+        return huber + sparsity * self.alpha, mse, sparsity, h, x_hat
+
+    @property
+    def dictionary(self):
+        return normalize_dict(self.embed).detach()
+
+class ConstrainedRICA(torch.nn.Module):
+    def __init__(self, d_in, d_hidden, alpha):
+        super().__init__()
+        initial_matrix = torch.randn(d_hidden, d_in)
+        initial_matrix = normalize_dict(initial_matrix)
+        self.embed = torch.nn.Parameter(initial_matrix.clone())
+        self.embed_bias = torch.nn.Parameter(torch.zeros(d_in))
+
+        self.alpha = alpha
+    
+    def forward(self, x):
+        with torch.no_grad():
+            self.embed.data = normalize_dict(self.embed.data)
+
+        h = torch.einsum('ij,bj->bi', self.embed, x - self.embed_bias.unsqueeze(0))
+        x_hat = torch.einsum('ij,bi->bj', self.embed, h) + self.embed_bias.unsqueeze(0)
+
+        mse = (x - x_hat).pow(2).mean()
+        sparsity = h.abs().mean()
+
+        return mse + sparsity * self.alpha, mse, sparsity, h, x_hat
+
+    @property
+    def dictionary(self):
+        return normalize_dict(self.embed).detach()
+
+class NonNegativeRICA(torch.nn.Module):
+    def __init__(self, d_in, d_hidden, alpha):
+        super().__init__()
+        initial_matrix = torch.randn(d_hidden, d_in)
+        initial_matrix = normalize_dict(initial_matrix)
+        self.embed = torch.nn.Parameter(initial_matrix.clone())
+        self.embed_bias = torch.nn.Parameter(torch.zeros(d_in))
+
+        self.alpha = alpha
+ 
+    def forward(self, x):
+        h = torch.einsum('ij,bj->bi', self.embed, x - self.embed_bias.unsqueeze(0))
+        h = torch.nn.functional.softplus(h)
+        x_hat = torch.einsum('ij,bi->bj', self.embed, h) + self.embed_bias.unsqueeze(0)
+
+        mse = (x - x_hat).pow(2).mean()
+        sparsity = h.abs().mean()
+
+        return mse + sparsity * self.alpha, mse, sparsity, h, x_hat
 
     @property
     def dictionary(self):
@@ -96,9 +176,9 @@ class UntiedRICA(torch.nn.Module):
         x_hat = torch.einsum('ij,bi->bj', normalize_dict(self.unembed), h) + self.unembed_bias.unsqueeze(0)
 
         mse = (x - x_hat).pow(2).mean()
-        sparsity = torch.norm(h, 1, dim=-1).mean() * self.alpha
+        sparsity = h.abs().mean()
 
-        return mse + sparsity, mse, h, x_hat
+        return mse + sparsity * self.alpha, mse, sparsity, h, x_hat
 
     @property
     def dictionary(self):
@@ -110,7 +190,72 @@ def shrinkage(x, a, threshold=20):
     scale = 1 - torch.exp(-exponent)
     return x * scale
 
-class ShrinkageSAE(torch.nn.Module):
+class NNMeanCenteredICA(torch.nn.Module):
+    def __init__(self, d_in, d_hidden, alpha):
+        super().__init__()
+        initial_matrix = torch.randn(d_hidden, d_in)
+        initial_matrix = normalize_dict(initial_matrix)
+        self.embed = torch.nn.Parameter(initial_matrix.clone())
+
+        self.alpha = alpha
+    
+    def forward(self, x):
+        with torch.no_grad():
+            self.embed.data = normalize_dict(self.embed.data)
+
+        # mean center the batch
+        x_mean = x.mean(dim=0)
+        x_centered = x - x_mean
+
+        # project the batch onto the dictionary
+        h = torch.einsum('ij,bj->bi', self.embed, x_centered)
+        h = torch.nn.functional.relu(h)
+
+        # reconstruct the batch
+        x_hat = torch.einsum('ij,bi->bj', self.embed, h) + x_mean
+
+        mse = (x - x_hat).pow(2).mean()
+        sparsity = h.abs().mean()
+
+        return mse + sparsity * self.alpha, mse, sparsity, h, x_hat
+
+    @property
+    def dictionary(self):
+        return normalize_dict(self.embed).detach()
+
+class MeanCenteredICA(torch.nn.Module):
+    def __init__(self, d_in, d_hidden, alpha):
+        super().__init__()
+        initial_matrix = torch.randn(d_hidden, d_in)
+        initial_matrix = normalize_dict(initial_matrix)
+        self.embed = torch.nn.Parameter(initial_matrix.clone())
+
+        self.alpha = alpha
+    
+    def forward(self, x):
+        with torch.no_grad():
+            self.embed.data = normalize_dict(self.embed.data)
+
+        # mean center the batch
+        x_mean = x.mean(dim=0)
+        x_centered = x - x_mean
+
+        # project the batch onto the dictionary
+        h = torch.einsum('ij,bj->bi', self.embed, x_centered)
+        h = shrinkage(h, self.embed)
+
+        # reconstruct the batch
+        x_hat = torch.einsum('ij,bi->bj', self.embed, h)
+
+        mse = (x_centered - x_hat).pow(2).mean()
+        huber = huber_loss(x_centered, x_hat, 1)        
+
+        # scale by mean activation (but detach to avoid backprop)
+        sparsity = h.abs().mean()
+
+        return huber + sparsity * self.alpha, mse, sparsity, h, x_hat
+
+class ShrinkageRICA(torch.nn.Module):
     def __init__(self, d_in, d_hidden, alpha):
         super().__init__()
         initial_matrix = torch.randn(d_hidden, d_in)
@@ -122,14 +267,14 @@ class ShrinkageSAE(torch.nn.Module):
         self.alpha = alpha
     
     def forward(self, x):
-        h = torch.einsum('ij,bj->bi', self.embed, x + self.embed_bias.unsqueeze(0))
+        h = torch.einsum('ij,bj->bi', self.embed, x - self.embed_bias.unsqueeze(0))
         h = shrinkage(h, self.embed_shrinkage.unsqueeze(0))
-        x_hat = torch.einsum('ij,bi->bj', self.embed, h) - self.embed_bias.unsqueeze(0)
+        x_hat = torch.einsum('ij,bi->bj', self.embed, h) + self.embed_bias.unsqueeze(0)
 
         mse = (x - x_hat).pow(2).mean()
-        sparsity = torch.norm(h, 1, dim=-1).mean() * self.alpha
+        sparsity = h.abs().mean()
 
-        return mse + sparsity, mse, h, x_hat
+        return mse + sparsity * self.alpha, mse, sparsity, h, x_hat
     
     @property
     def dictionary(self):
--- training_sae.py
+++ training_sae.py
@@ -1,7 +1,7 @@
-from modelling_sae import BasicSAE, RICA, UntiedRICA, ShrinkageSAE
+from modelling_sae import BasicSAE, RICA, UntiedRICA, ShrinkageRICA, ConstrainedRICA, SoftplusSAE, NonNegativeRICA
 from cache_transformer_activations import cache_transformer_activations, ActivationCache
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 import torch
 from typing import List
 import tqdm
@@ -12,6 +12,69 @@ import dotenv
 
 dotenv.load_dotenv()
 hf_token = os.getenv("HUGGINGFACE_API_KEY")
+
+def load_and_process_dataset(
+    dataset_name: str,
+    tokenizer: AutoTokenizer,
+    max_length: int,
+):
+    """
+    Load, tokenize, and pack the dataset into sequences of a fixed length.
+    """
+
+    dataset = load_dataset(dataset_name, split="train")
+    
+    # tokenize the entire dataset
+    dataset = dataset.map(
+        lambda x: tokenizer(x["text"]),
+        batched=True,
+        remove_columns=["text"],
+    )
+
+    def split_dataset(dataset, chunk_size):
+        """
+        Split the dataset into chunks of a fixed length.
+        """
+        chunks = []
+        for i in tqdm.tqdm(range(len(dataset))):
+            packed_entry = dataset[i]["input_ids"]
+            for j in range(0, len(packed_entry), chunk_size):
+                chunk = packed_entry[j:j + chunk_size]
+                if len(chunk) == chunk_size:
+                    chunks.append(chunk)
+                else:
+                    continue
+        return chunks
+
+    dataset_chunks = split_dataset(dataset, max_length)
+    
+    # reprocess into a hf dataset
+    dataset = Dataset.from_dict({
+        "input_ids": dataset_chunks,
+    })
+    dataset.set_format(type="torch", columns=["input_ids"])
+
+    return dataset
+
+def optimizer_to(optim, device):
+    for param in optim.state.values():
+        # Not sure there are any global tensors in the state dict
+        if isinstance(param, torch.Tensor):
+            param.data = param.data.to(device)
+            if param._grad is not None:
+                param._grad.data = param._grad.data.to(device)
+        elif isinstance(param, dict):
+            for subparam in param.values():
+                if isinstance(subparam, torch.Tensor):
+                    subparam.data = subparam.data.to(device)
+                    if subparam._grad is not None:
+                        subparam._grad.data = subparam._grad.data.to(device)
+
+def move_everything_to_device(models, optimizers, device):
+    for model in models:
+        model.to(device)
+    for optimizer in optimizers:
+        optimizer_to(optimizer, device)
 
 def train_on_cache(
     models: List[torch.nn.Module],
@@ -39,6 +102,7 @@ def train_on_cache(
         sparsity_running_mean = [0 for _ in models]
         loss_running_mean = [0 for _ in models]
         mse_running_mean = [0 for _ in models]
+        l1_running_mean = [0 for _ in models]
 
         cache_running_mean = torch.zeros_like(cache.activations[layer][0][0]).to(device)
         mse_scale_running_mean = 0
@@ -69,30 +133,36 @@ def train_on_cache(
             mse_scale_running_mean = (mse_scale_running_mean * batch_idx + mse_scale) / (batch_idx + 1)
 
             for model_idx, (model, optimizer, scheduler, name) in enumerate(zip(models, optimizers, schedulers, model_names)):
-                optimizer.zero_grad()
-                loss, mse, h, x_hat = model(current_batch)
-                loss.backward()
-                optimizer.step()
-                scheduler.step()
+                def step():
+                    optimizer.zero_grad()
+                    loss, mse, l1, h, x_hat = model(current_batch)
+                    loss.backward()
+                    optimizer.step()
+                    scheduler.step()
 
-                loss_running_mean[model_idx] += loss.item()
-                mse_running_mean[model_idx] += mse.item()
-                sparsity_running_mean[model_idx] += (h.abs() > 0).float().sum(dim=-1).mean().item()
+                    loss_running_mean[model_idx] += loss.item()
+                    mse_running_mean[model_idx] += mse.item()
+                    sparsity_running_mean[model_idx] += (h.abs() > 0).float().sum(dim=-1).mean().item()
+                    l1_running_mean[model_idx] += l1.item()
 
-                metrics[name] = {}
+                    metrics[name] = {}
 
-                if log_loss_interval != 0 and batch_idx % log_loss_interval == log_loss_interval - 1:
-                    loss_running_mean[model_idx] /= log_loss_interval
-                    metrics[name]["loss"] = loss_running_mean[model_idx]
-                    mse_running_mean[model_idx] /= log_loss_interval
-                    metrics[name]["mse"] = mse_running_mean[model_idx]
-                    metrics[name]["scaled_mse"] = mse_running_mean[model_idx] / mse_scale_running_mean
-                    sparsity_running_mean[model_idx] /= log_loss_interval
-                    metrics[name]["sparsity"] = sparsity_running_mean[model_idx]
-                    metrics[name]["lr"] = optimizer.param_groups[0]["lr"]
-                    loss_running_mean[model_idx] = 0
-                    mse_running_mean[model_idx] = 0
-                    sparsity_running_mean[model_idx] = 0
+                    if log_loss_interval != 0 and batch_idx % log_loss_interval == log_loss_interval - 1:
+                        loss_running_mean[model_idx] /= log_loss_interval
+                        metrics[name]["loss"] = loss_running_mean[model_idx]
+                        mse_running_mean[model_idx] /= log_loss_interval
+                        metrics[name]["mse"] = mse_running_mean[model_idx]
+                        metrics[name]["scaled_mse"] = mse_running_mean[model_idx] / mse_scale_running_mean
+                        sparsity_running_mean[model_idx] /= log_loss_interval
+                        metrics[name]["sparsity"] = sparsity_running_mean[model_idx]
+                        metrics[name]["lr"] = optimizer.param_groups[0]["lr"]
+                        l1_running_mean[model_idx] /= log_loss_interval
+                        metrics[name]["l1"] = l1_running_mean[model_idx]
+                        loss_running_mean[model_idx] = 0
+                        mse_running_mean[model_idx] = 0
+                        sparsity_running_mean[model_idx] = 0
+                
+                step()
 
             if log_loss_interval != 0 and batch_idx % log_loss_interval == log_loss_interval - 1:
                 wandb.log(metrics, step=batch_idx + start_batch_count, commit=True)
@@ -124,17 +194,19 @@ def interleave_training_and_generation(
 
     total_steps = 0
 
-    dataset = load_dataset(dataset_name, split="train")
-    dataset = dataset.map(
-        lambda x: tokenizer(x["text"], padding="max_length", truncation=True, max_length=sample_max_length),
-        batched=True,
-        remove_columns=["text"],
+    dataset = load_and_process_dataset(
+        dataset_name=dataset_name,
+        tokenizer=tokenizer,
+        max_length=sample_max_length,
     )
-    dataset.set_format(type="torch", columns=["input_ids"])
+
+    print("Total # tokens in dataset:", len(dataset) * sample_max_length)
 
     total_processed_sentences = 0
 
     for epoch in range(num_epochs):
+        move_everything_to_device(models, optimizers, "cpu")
+
         model = AutoModelForCausalLM.from_pretrained(**model_kwargs).to(device)
 
         cache, processed_sentences = cache_transformer_activations(
@@ -151,6 +223,8 @@ def interleave_training_and_generation(
         )
 
         del model
+
+        move_everything_to_device(models, optimizers, device)
 
         total_steps += train_on_cache(
             models=models,
@@ -184,27 +258,22 @@ def interleave_training_and_generation(
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    l1_alphas = np.logspace(-5, 0, 6)
+    l1_alphas = [1e-5, 1e-3, 1e-1]
 
     hidden_size = 2048
-    ratio = 32
+    ratio = 16
 
     models = [
-        BasicSAE(hidden_size, hidden_size * ratio, alpha) for alpha in l1_alphas
+        UntiedRICA(hidden_size, hidden_size * ratio, alpha) for alpha in l1_alphas
     ] + [
         RICA(hidden_size, hidden_size * ratio, alpha) for alpha in l1_alphas
     ]
-    # ] + [
-    #     UntiedRICA(hidden_size, hidden_size * ratio, alpha) for alpha in l1_alphas
-    # ] + [
-    #     ShrinkageSAE(hidden_size, hidden_size * ratio, alpha) for alpha in l1_alphas
-    # ]
 
     models = [model.to(device) for model in models]
 
     optimizers = [torch.optim.Adam(model.parameters(), lr=4e-4) for model in models]
-    # linear warmup
 
+    # linear warmup
     def linear_warmup(num_warmup_steps):
         def f(epoch):
             if epoch < num_warmup_steps:
@@ -212,22 +281,27 @@ if __name__ == "__main__":
             return 1
 
         return f
+    
+    def linear_warmup_cooldown(num_warmup_steps, num_cooldown_steps):
+        def f(epoch):
+            if epoch < num_warmup_steps:
+                return epoch / num_warmup_steps
+            if epoch < num_warmup_steps + num_cooldown_steps:
+                return max(1 - (epoch - num_warmup_steps) / num_cooldown_steps, 0)
+            return 0
+
+        return f
 
     schedulers = [
-        torch.optim.lr_scheduler.LambdaLR(optimizer, linear_warmup(1000))
+        torch.optim.lr_scheduler.LambdaLR(optimizer, linear_warmup_cooldown(1000, 100_000))
         for optimizer in optimizers
     ]
 
     model_names = [
-        f"basic_sae_{alpha:.2e}" for alpha in l1_alphas
+        f"UntiedRICA_{alpha:.2e}" for alpha in l1_alphas
     ] + [
-        f"rica_{alpha:.2e}" for alpha in l1_alphas
+        f"RICA_{alpha:.2e}" for alpha in l1_alphas
     ]
-    # ] + [
-    #     f"untied_rica_{alpha:.2e}" for alpha in l1_alphas
-    # ] + [
-    #     f"shrinkage_sae_{alpha:.2e}" for alpha in l1_alphas
-    # ]
 
     model_kwargs = {
         "pretrained_model_name_or_path": "EleutherAI/pythia-1.4b",
@@ -242,12 +316,12 @@ if __name__ == "__main__":
         model_names=model_names,
         tokenizer=AutoTokenizer.from_pretrained("EleutherAI/pythia-1.4b"),
         model_kwargs=model_kwargs,
-        dataset_name="monology/pile-uncopyrighted",
+        dataset_name="Elriggs/openwebtext-100k",
         device=device,
         num_epochs=10,
         num_cache_epochs=1,
-        sample_batch_size=64,
+        sample_batch_size=32,
         sample_max_length=256,
         train_batch_size=4096,
-        layer=8,
+        layer=16,
     )
